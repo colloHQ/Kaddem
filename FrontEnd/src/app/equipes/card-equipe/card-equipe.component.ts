8ab4c22bf468b326912b1ba5b7c30b4bb3f2140b
Wassim Ben R <wassimbenr@gmail.com>
2022-12-13 23:52:07 +0100

    list Equipe

--- FrontEnd/src/app/equipes/card-equipe/card-equipe.component.ts
+++ FrontEnd/src/app/equipes/card-equipe/card-equipe.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Equipes } from './../../core/model/equipes';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-equipe',
   templateUrl: './card-equipe.component.html',
-  styleUrls: ['./card-equipe.component.scss']
+  styleUrls: ['./card-equipe.component.scss'],
 })
 export class CardEquipeComponent implements OnInit {
+  @Input() currentEquipe: Equipes;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- FrontEnd/src/app/equipes/form-equipe/form-equipe.component.ts
+++ FrontEnd/src/app/equipes/form-equipe/form-equipe.component.ts
@@ -1,15 +1,40 @@
+import { EquipesService } from './../../core/service/equipes.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-equipe',
   templateUrl: './form-equipe.component.html',
-  styleUrls: ['./form-equipe.component.scss']
+  styleUrls: ['./form-equipe.component.scss'],
 })
 export class FormEquipeComponent implements OnInit {
+  equipeForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder,
+    private router: Router,
+    public equipesService: EquipesService
+  ) {}
 
   ngOnInit(): void {
+    this.equipeForm = this.fb.group({
+      nomEquipe: [''],
+      niveau: [''],
+      photos: [''],
+    });
+  }
+  verifyForm() {
+    console.log(this.equipeForm.value.nomEquipe);
+    console.log(this.equipeForm.value.niveau);
+    console.log(this.equipeForm.value.photos);
+    //cnx backend
   }
 
+  submitForm() {
+    this.equipesService.addEquipe(this.equipeForm.value).subscribe((res) => {
+      console.log('Equipe created!');
+      this.router.navigateByUrl('/equipes/list');
+    });
+  }
 }
